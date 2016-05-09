68483168904495845493c2b687fb0e25dc3783cb
mconway <mconway@ils00213.dhcp.unc.edu>
2016-05-09 14:06:49 -0400

    #2 added configurable entitlement source

---
+++ src/test/java/org/iplantc/cas/persondir/ConfigurableEntitlementResolverTest.java
@@ -0,0 +1,36 @@
+package org.iplantc.cas.persondir;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jasig.services.persondir.IPersonAttributes;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ConfigurableEntitlementResolverTest {
+
+	@Test
+	public void testGetAdminGroupsGivenUser() {
+
+		String testuser1 = "testuser1";
+		String testuser2 = "testuser2";
+		String entitlement1 = "entitlement1";
+		String entitlement2 = "entitlement2";
+
+		List<String> userNames = new ArrayList<String>();
+		userNames.add(testuser1);
+		userNames.add(testuser2);
+
+		List<String> entitlements = new ArrayList<String>();
+		entitlements.add(entitlement1);
+		entitlements.add(entitlement2);
+
+		ConfigurableEntitlementResolver resolver = new ConfigurableEntitlementResolver();
+		resolver.setConfiguredEntitlements(entitlements);
+		resolver.setConfiguredUserIds(userNames);
+		IPersonAttributes actual = resolver.getPerson(testuser1);
+		Assert.assertNotNull("Null personAttributes returned", actual);
+
+	}
+
+}
